52425ba64cd54cd828145058a654536b76cac3d0
amaalyy <135268152+amaalyy@users.noreply.github.com>
2025-02-01 22:41:58 +0100

    fix error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Increase the body size limit here
 app.use(cookieParser());
 app.use(
   cors({
